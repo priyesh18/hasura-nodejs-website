3938048bd8cea1c1d5369bce1150e76c05f06ce2
PriyeshPatel <priyesh@example.com>
2017-06-26 18:15:18 +0530

    h

--- app/server.js
+++ app/server.js
@@ -1,4 +1,4 @@
-var express = require('express');
+/*var express = require('express');
 var app = express();
 var fetch = require('node-fetch');
 
@@ -71,7 +71,7 @@ app.get('/', function (req, res) {
  * To test, login to the console as an admin user. /admin, /user will work.
  * Make a request to /admin, /user from an incognito tab. They won't work, only /anonymous will work.
  */
-app.get('/:role', function (req, res) {
+/*app.get('/:role', function (req, res) {
   var roles = req.get('X-Hasura-Allowed-Roles');
 
   // Check if allowed roles contains the rolename mentioned in the URL
@@ -84,4 +84,89 @@ app.get('/:role', function (req, res) {
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
+});
+*/
+var express = require("express"),
+    bodyParser = require("body-parser"), //get the params from the request body
+    app = express();
+var fetch = require('node-fetch');
+
+
+app.set("view engine", "ejs"); //so that I don't have to write .ejs again and again
+app.use(express.static("public")); //serve the contents of my public directory
+app.use(bodyParser.urlencoded({ extended: true }));
+//Vars
+var dataurl = "http://data.c100.hasura.me";
+var authurl = "http://auth.c100.hasura.me";
+var headers = { 'Content-Type': 'application/json' };
+//Routes
+app.get('/', function(req, res) {
+    var schemaFetchUrl = dataurl + '/v1/query';
+    var options = {
+        method: 'POST',
+        headers,
+        body: JSON.stringify({
+            type: 'select',
+            args: {
+                table: 'topic',
+                columns: ['*'],
+
+            }
+        })
+    };
+    fetch(schemaFetchUrl, options)
+      .then(function(res) {
+        return res.json();
+    }).then(function(json) {
+        res.render("home",{data:json});
+    });
+        /*.then(
+            (response) => {
+                response.text()
+                    .then(
+                        (body) => {
+                            var data = JSON.parse(body);
+                            res.render("home", { data: data });
+                            
+                        },
+                        (e) => {
+                            res.send('Error in fetching current schema: ' + err.toString());
+                        })
+                    .catch((e) => {
+                        e.stack();
+                        res.send('Error in fetching current schema: ' + e.toString());
+                    });
+            },
+            (e) => {
+                console.error(e);
+                res.send('Error in fetching current schema: ' + e.toString());
+            })
+        .catch((e) => {
+            e.stackTrace();
+            res.send('Error in fetching current schema: ' + e.toString());
+        });*/
+});
+
+
+
+app.get('/signup', function(req, res) {
+
+});
+
+app.post('/signup', function(req, res) {
+
+});
+
+app.get('/login', function(req, res) {
+
+});
+
+app.post('/login', function(req, res) {
+
+});
+
+
+
+app.listen(8080, function() {
+    console.log("listening on port 8080");
 });
