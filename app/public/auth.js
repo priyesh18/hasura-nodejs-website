936416d8a7d5cfb9c91189987430dc6d60310279
PriyeshPatel <priyesh@example.com>
2017-07-07 12:08:47 +0530

    change url

--- app/public/auth.js
+++ app/public/auth.js
@@ -1,10 +1,10 @@
 $('[data-toggle="tooltip"]').tooltip();
 
 //Auth
-var auth_url = "http://auth.c100.hasura.me";
-var data_url = "http://data.c100.hasura.me";
-//var auth_url = "http://auth.priyesh.hasura.me";
-//var data_url = "http://data.priyesh.hasura.me";
+//var auth_url = "http://auth.c100.hasura.me";
+//var data_url = "http://data.c100.hasura.me";
+var auth_url = "http://auth.priyesh.hasura.me";
+var data_url = "http://data.priyesh.hasura.me";
 (Cookies.get('id') == undefined) ? $('#out').css("display", "none"): $('#out').css("display", "block");
 (Cookies.get('id') == undefined) ? $('#in').css("display", "block"): $('#in').css("display", "none");
 
